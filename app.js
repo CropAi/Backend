af8062ea176a8fde4511fd1f0e6c707fd96aed45
Tanishq Saluja <tanishqsaluja18@gmail.com>
2020-03-30 11:29:35 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/',express.static('public'))
-
-app.listen(3333,function(){
-    console.log('Server Working, Ctrl+C to stop server')
-})
